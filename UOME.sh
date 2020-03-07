acc4a5ca51d63ab59130061c41050458a7c562fe
Rasengangstarr <davidouthere@msn.com>
2020-03-07 13:02:44 +0000

    added ability to list the transactions for a given person

--- UOME.sh
+++ UOME.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-re='^[0-9]+([.][0-9]+)?$'
+re='^[+-]?[0-9]+([.][0-9]+)?$'
 
 if [ $# -lt 1 ]
 then
 	echo "help placeholder"
 fi
 
-if [ "$1" == "-l" ]
+if [ "$1" == "-l" ] && [ "$2" == "" ]
 then
-find . -name '*.txt' -printf "%f " -exec awk '{ sum += $1 } END{ print sum }' {} \; | sed "s/\.txt//"
+	find . -name '*.txt' -printf "%f " -exec awk '{ sum += $1 } END{ print sum }' {} \; | sed "s/\.txt//"
+
+elif [ "$1" == "-l" ] && [ "$2" != "" ]
+then
+	cat $2.txt
 
 elif [ "$1" != "" ] && [ "$2" != "" ] && [[ $2 =~ $re ]]
 then
